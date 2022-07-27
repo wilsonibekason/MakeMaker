30548a9dbf7505ac2110617358418569a46469ad
wilsonibekason <wanbekgithub@gmail.com>
2022-07-27 11:22:06 -0700

    add env variable and initialised sanity studio

--- src/client.js
+++ src/client.js
@@ -1,2 +1,14 @@
+import sanityClient from '@sanity/client';
+import imageUrlBuilder from '@sanity/image-url';
 
-import 
+export const client = sanityClient({
+    projectId: process.env.SANITY_PROJECTID,
+    dataset: 'production',
+    apiVersion: '2022-02-01',
+    useCdn: true,
+   token: process.env.SANITY_API_TOKEN, 
+});
+
+const builder = imageUrlBuilder(client);
+
+export const  urlFor = (source) => builder.image(source);
