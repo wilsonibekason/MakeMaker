8669bfaa9bd7bd79c115a7204b19c083b526cfe2
wilsonibekason <wanbekgithub@gmail.com>
2022-07-29 09:16:05 -0600

    complete reachout schema

---
+++ makemaker/schemas/documents/ReachOut.js
@@ -0,0 +1,37 @@
+export default {
+  name: "reactOut",
+  title: "Reach Out",
+  description: "A reach out of the world.",
+  type: "document",
+  icon: "",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "text",
+      required: true,
+      minLength: 1,
+      maxLength: 100,
+      description: "This should be assigned only once in the schema",
+    },
+    {
+      name: "description",
+      title: "Description",
+      type: "text",
+      required: true,
+      minLength: 1,
+      maxLength: 100,
+      description: "This should be assigned only once in the schema",
+    },
+    {
+      name: "image",
+      title: "Image",
+      type: "image",
+      required: true,
+      minLength: 1,
+      maxLength: 100,
+      description: "This should be assigned only once in the schema",
+      options: { hotspot: true },
+    },
+  ],
+};
