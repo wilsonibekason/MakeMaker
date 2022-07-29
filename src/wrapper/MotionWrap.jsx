83f090b41c9b5a94e3dceb6a8cd1bc955a816950
wilsonibekason <wanbekgithub@gmail.com>
2022-07-29 07:19:02 -0600

    reinitialised motionWrap

--- src/wrapper/MotionWrap.jsx
+++ src/wrapper/MotionWrap.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { motion } from "framer-motion";
 
-const MotionWrap = (Component) =>
+const MotionWrap = (Component, classNames) =>
   function HOC() {
     return (
       <motion.div
         whileInView={{ y: [100, 50, 0], opacity: [0, 0, 1] }}
-        transition={{ duration: 0.6, ease: "easeInOut" }}
+        transition={{ duration: 0.5 }}
+        className={`${classNames} app__flex`}
       >
         <Component />
       </motion.div>
