901627ba088cbba3ca3b0ee0a2415755d859679d
wilsonibekason <wanbekgithub@gmail.com>
2022-07-30 16:11:45 -0600

    complete integrate context for aboutsection

--- src/oncontext/aboutContext/onAboutContext.js
+++ src/oncontext/aboutContext/onAboutContext.js
@@ -12,6 +12,7 @@ const AboutContext = createContext({});
 export const AboutProvider = ({ children }) => {
   const [aboutHeader, setAboutHeader] = useState();
   const [sectionContents, setSectionContents] = useState([]);
+  const [aboutSection, setAboutSection] = useState();
 
   useEffect(() => {
     // querying for aboutHeader
@@ -20,6 +21,9 @@ export const AboutProvider = ({ children }) => {
     // querying for sectionHeader
     const sectionContentQuery = '*[_type == "sectionContent"]';
     client.fetch(sectionContentQuery).then((data) => setSectionContents(data));
+    // querying fot about section contents
+    const aboutSectioonQuery = '*[_type == "aboutSection"]';
+    client.fetch(aboutSectioonQuery).then((data) => setAboutSection(data));
   }, []);
 
   // destructuring for section contents
@@ -27,10 +31,22 @@ export const AboutProvider = ({ children }) => {
   const sectionItem2 = sectionContents[1];
   const sectionItem3 = sectionContents[2];
   const sectionItem4 = sectionContents[3];
-
+  // destructuring for about section
+  const sectionTitle = aboutSection?.map((item) => item?.articleTitle);
+  const sectionDesc = aboutSection?.map((item) => item?.articleDesc);
+  const sectionImage = aboutSection?.map((item) => urlFor(item?.cardImgUrl));
+  const sectionAuthor = aboutSection?.map((item) => item?.companyName);
+  const cardTitle = aboutSection?.map((item) => item?.cardTitle);
+  const cardDesc = aboutSection?.map((item) => item?.cardDesc);
   return (
     <AboutContext.Provider
-      value={{ aboutHeader, sectionItem1, sectionItem2, sectionItem3 }}
+      value={{
+        aboutHeader,
+        sectionItem1,
+        sectionItem2,
+        sectionItem3,
+        aboutSection,
+      }}
     >
       {children}
     </AboutContext.Provider>
