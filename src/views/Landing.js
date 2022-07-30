8324e0ca77c40702fe9614011a9fa8f5789ff474
wilsonibekason <wanbekgithub@gmail.com>
2022-07-30 16:18:15 -0600

    complete add about section to client

--- src/views/Landing.js
+++ src/views/Landing.js
@@ -16,6 +16,7 @@ export default function Landing() {
     sectionItem2,
     sectionItem3,
     sectionItem4,
+    aboutSection,
   } = useStateContextAbout();
   const { sectionContents } = useStateContext();
   console.log(sectionItem1);
@@ -129,62 +130,70 @@ export default function Landing() {
                 </div>
               </div>
             </div>
-
-            <div className="flex flex-wrap items-center mt-32">
-              <div className="w-full md:w-5/12 px-4 mr-auto ml-auto">
-                <div className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-white">
-                  <i className="fas fa-user-friends text-xl"></i>
-                </div>
-                <h3 className="text-3xl mb-2 font-semibold leading-normal">
-                  Working with us is a pleasure
-                </h3>
-                <p className="text-lg font-light leading-relaxed mt-4 mb-4 text-blueGray-600">
-                  Don't let your uses guess by attaching tooltips and popoves to
-                  any element. Just make sure you enable them first via
-                  JavaScript.
-                </p>
-                <p className="text-lg font-light leading-relaxed mt-0 mb-4 text-blueGray-600">
-                  The kit comes with three pre-built pages to help you get
-                  started faster. You can change the text and images and you're
-                  good to go. Just make sure you enable them first via
-                  JavaScript.
-                </p>
-                <Link to="/" className="font-bold text-blueGray-700 mt-8">
-                  Check Notus React!
-                </Link>
-              </div>
-
-              <div className="w-full md:w-4/12 px-4 mr-auto ml-auto">
-                <div className="relative flex flex-col min-w-0 break-words  w-full mb-6 shadow-lg rounded-lg bg-lightBlue-500">
-                  <img
-                    alt="..."
-                    src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80"
-                    className="w-full align-middle rounded-t-lg"
-                  />
-                  <blockquote className="relative p-8 mb-4">
-                    <svg
-                      preserveAspectRatio="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 583 95"
-                      className="absolute left-0 w-full block h-95-px -top-94-px"
-                    >
-                      <polygon
-                        points="-30,95 583,95 583,65"
-                        className="text-lightBlue-500 fill-current"
-                      ></polygon>
-                    </svg>
-                    <h4 className="text-xl font-bold text-white">
-                      Top Notch Services
-                    </h4>
-                    <p className="text-md font-light mt-2 text-white">
-                      The Arctic Ocean freezes every winter and much of the
-                      sea-ice then thaws every summer, and that process will
-                      continue whatever happens.
+            {aboutSection?.map((item, index) => {
+              const {
+                articleTitle,
+                articleDesc,
+                companyName,
+                cardTitle,
+                cardDesc,
+                cardImgUrl,
+              } = item;
+              return (
+                <div className="flex flex-wrap items-center mt-32">
+                  <div className="w-full md:w-5/12 px-4 mr-auto ml-auto">
+                    <div className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-white">
+                      <i className="fas fa-user-friends text-xl"></i>
+                    </div>
+                    <h3 className="text-3xl mb-2 font-semibold leading-normal">
+                      {articleTitle}
+                    </h3>
+                    <p className="text-lg font-light leading-relaxed mt-4 mb-4 text-blueGray-600">
+                      {articleDesc}
                     </p>
-                  </blockquote>
+                    <p className="text-lg font-light leading-relaxed mt-0 mb-4 text-blueGray-600">
+                      The kit comes with three pre-built pages to help you get
+                      started faster. You can change the text and images and
+                      you're good to go. Just make sure you enable them first
+                      via JavaScript.
+                    </p>
+                    <Link to="/" className="font-bold text-blueGray-700 mt-8">
+                      {companyName}
+                    </Link>
+                  </div>
+
+                  <div className="w-full md:w-4/12 px-4 mr-auto ml-auto">
+                    <div className="relative flex flex-col min-w-0 break-words  w-full mb-6 shadow-lg rounded-lg bg-lightBlue-500">
+                      <img
+                        alt="..."
+                        //src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80"
+                        src={urlFor(cardImgUrl)}
+                        className="w-full align-middle rounded-t-lg"
+                      />
+                      <blockquote className="relative p-8 mb-4">
+                        <svg
+                          preserveAspectRatio="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 583 95"
+                          className="absolute left-0 w-full block h-95-px -top-94-px"
+                        >
+                          <polygon
+                            points="-30,95 583,95 583,65"
+                            className="text-lightBlue-500 fill-current"
+                          ></polygon>
+                        </svg>
+                        <h4 className="text-xl font-bold text-white">
+                          {cardTitle}
+                        </h4>
+                        <p className="text-md font-light mt-2 text-white">
+                          {cardTitle}
+                        </p>
+                      </blockquote>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
+              );
+            })}
           </div>
         </section>
 
