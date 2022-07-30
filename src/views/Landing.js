593809a252872d0afdb2260d0f92507028b40f15
wilsonibekason <wanbekgithub@gmail.com>
2022-07-30 15:20:18 -0600

    complete add makemaker aboutheader

--- src/views/Landing.js
+++ src/views/Landing.js
@@ -5,61 +5,68 @@ import { Link } from "react-router-dom";
 
 import Navbar from "components/Navbars/AuthNavbar.js";
 import Footer from "components/Footers/Footer.js";
+import { useStateContext } from "../oncontext/aboutContext/onAboutContext";
+import { urlFor } from "client";
 
 export default function Landing() {
+  const { aboutHeader } = useStateContext();
   return (
     <>
       <Navbar transparent />
       <main>
-        <div className="relative pt-16 pb-32 flex content-center items-center justify-center min-h-screen-75">
-          <div
-            className="absolute top-0 w-full h-full bg-center bg-cover"
-            style={{
-              backgroundImage:
-                "url('https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80')",
-            }}
-          >
-            <span
-              id="blackOverlay"
-              className="w-full h-full absolute opacity-75 bg-black"
-            ></span>
-          </div>
-          <div className="container relative mx-auto">
-            <div className="items-center flex flex-wrap">
-              <div className="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center">
-                <div className="pr-12">
-                  <h1 className="text-white font-semibold text-5xl">
-                    Your story starts with us.
-                  </h1>
-                  <p className="mt-4 text-lg text-blueGray-200">
-                    This is a simple example of a Landing Page you can build
-                    using Notus React. It features multiple CSS components based
-                    on the Tailwind CSS design system.
-                  </p>
+        {aboutHeader?.map((item, index) => {
+          const { image, description, title } = item;
+          return (
+            <div className="relative pt-16 pb-32 flex content-center items-center justify-center min-h-screen-75">
+              <div
+                className="absolute top-0 w-full h-full bg-center bg-cover"
+                style={{
+                  // backgroundImage:
+                  //   "url('https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80')",
+                  backgroundImage: `url(${urlFor(item?.image)})`,
+                }}
+              >
+                <span
+                  id="blackOverlay"
+                  className="w-full h-full absolute opacity-75 bg-black"
+                ></span>
+              </div>
+              <div className="container relative mx-auto">
+                <div className="items-center flex flex-wrap">
+                  <div className="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center">
+                    <div className="pr-12">
+                      <h1 className="text-white font-semibold text-5xl">
+                        {title}
+                      </h1>
+                      <p className="mt-4 text-lg text-blueGray-200">
+                        {description}
+                      </p>
+                    </div>
+                  </div>
                 </div>
               </div>
+              <div
+                className="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden h-70-px"
+                style={{ transform: "translateZ(0)" }}
+              >
+                <svg
+                  className="absolute bottom-0 overflow-hidden"
+                  xmlns="http://www.w3.org/2000/svg"
+                  preserveAspectRatio="none"
+                  version="1.1"
+                  viewBox="0 0 2560 100"
+                  x="0"
+                  y="0"
+                >
+                  <polygon
+                    className="text-blueGray-200 fill-current"
+                    points="2560 0 2560 100 0 100"
+                  ></polygon>
+                </svg>
+              </div>
             </div>
-          </div>
-          <div
-            className="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden h-70-px"
-            style={{ transform: "translateZ(0)" }}
-          > 
-            <svg
-              className="absolute bottom-0 overflow-hidden"
-              xmlns="http://www.w3.org/2000/svg"
-              preserveAspectRatio="none"
-              version="1.1"
-              viewBox="0 0 2560 100"
-              x="0"
-              y="0"
-            >
-              <polygon
-                className="text-blueGray-200 fill-current"
-                points="2560 0 2560 100 0 100"
-              ></polygon>
-            </svg>
-          </div>
-        </div>
+          );
+        })}
 
         <section className="pb-20 bg-blueGray-200 -mt-24">
           <div className="container mx-auto px-4">
