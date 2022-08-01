2d45050256bdfa2f69c1d3a7c8b026d7a2cdcee7
Wilson Ibekason <wanbekgithub@gmail.com>
2022-08-01 07:54:25 -0600

    COMPLETE CONTENT SHEMA AND CONTENT

--- src/views/Landing.js
+++ src/views/Landing.js
@@ -30,6 +30,7 @@ export default function Landing() {
     message,
     handkeChangeInput,
     loading,
+    contactCard,
   } = useStateContextAbout();
   const { sectionContents } = useStateContext();
   console.log(sectionItem1);
@@ -404,42 +405,22 @@ export default function Landing() {
               </div>
             </div>
             <div className="flex flex-wrap mt-12 justify-center">
-              <div className="w-full lg:w-3/12 px-4 text-center">
-                <div className="text-blueGray-800 p-3 w-12 h-12 shadow-lg rounded-full bg-white inline-flex items-center justify-center">
-                  <i className="fas fa-medal text-xl"></i>
-                </div>
-                <h6 className="text-xl mt-5 font-semibold text-white">
-                  Excelent Services
-                </h6>
-                <p className="mt-2 mb-4 text-blueGray-400">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-              </div>
-              <div className="w-full lg:w-3/12 px-4 text-center">
-                <div className="text-blueGray-800 p-3 w-12 h-12 shadow-lg rounded-full bg-white inline-flex items-center justify-center">
-                  <i className="fas fa-poll text-xl"></i>
-                </div>
-                <h5 className="text-xl mt-5 font-semibold text-white">
-                  Grow your market
-                </h5>
-                <p className="mt-2 mb-4 text-blueGray-400">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-              </div>
-              <div className="w-full lg:w-3/12 px-4 text-center">
-                <div className="text-blueGray-800 p-3 w-12 h-12 shadow-lg rounded-full bg-white inline-flex items-center justify-center">
-                  <i className="fas fa-lightbulb text-xl"></i>
-                </div>
-                <h5 className="text-xl mt-5 font-semibold text-white">
-                  Launch time
-                </h5>
-                <p className="mt-2 mb-4 text-blueGray-400">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-              </div>
+              {contactCard?.map((item, index) => {
+                const { cardName, cardDesc, cardIcon } = item;
+                return (
+                  <>
+                    <div className="w-full lg:w-3/12 px-4 text-center">
+                      <div className="text-blueGray-800 p-3 w-12 h-12 shadow-lg rounded-full bg-white inline-flex items-center justify-center">
+                        <i className={`fas ${cardIcon} text-xl`}></i>
+                      </div>
+                      <h6 className="text-xl mt-5 font-semibold text-white">
+                        {cardName}
+                      </h6>
+                      <p className="mt-2 mb-4 text-blueGray-400">{cardDesc}</p>
+                    </div>
+                  </>
+                );
+              })}
             </div>
           </div>
         </section>
