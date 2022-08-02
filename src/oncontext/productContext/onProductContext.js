dac13548c0d15619d3cca71b3d75ee237b3e2154
Wilson Ibekason <wanbekgithub@gmail.com>
2022-08-02 11:46:15 +0100

    complete usereducer text case interface

--- src/oncontext/productContext/onProductContext.js
+++ src/oncontext/productContext/onProductContext.js
@@ -1,10 +1,46 @@
-import React, { useState, useEffect, useContext, createContext } from "react";
+import React, {
+  useState,
+  useEffect,
+  useContext,
+  createContext,
+  useReducer,
+} from "react";
 import { urlFor } from "client";
 
 const ProductContext = createContext({});
 
 export const ProductProvider = ({ children }) => {
   const [products, setProducts] = useState([]);
+  const [productBanner, setProductsBanner] = useState([]);
+  const [productTitle, setProductTitle] = useState("");
+  const [productDescription, setProductDescription] = useState("");
+  const [productImage, setProductImage] = useState("");
+  const [productPrice, setProductPrice] = useState("");
+  const [productIsLoading, setProductIsLoading] = useState(false);
+  const [productIsLoaded, setProductIsLoaded] = useState(false);
+  const [productError, setProductError] = useState(false);
+  const [productSuccess, setProductSuccess] = useState(false);
+  const [productErrorMsg, setProductErrorMsg] = useState("");
+  const [productSuccessMsg, setProductSuccessMsg] = useState("");
+  // declaring reducer actions
+  const ACTIONS = {
+    INCREMENT: "increment",
+    DECREMENT: "decrement",
+  };
+  const { INCREMENT, DECREMENT } = ACTIONS;
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case INCREMENT:
+        return { count: state.count + 1 };
+        break;
+      case DECREMENT:
+        return { count: state.count };
+    }
+  };
+  // TRYING OUT THE USErEDUUCER
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
+  // calling the function
+  dispatch({ type: DECREMENT });
   return (
     <ProductContext.Provider value={{}}>{children}</ProductContext.Provider>
   );
--- src/views/products.jsx
+++ src/views/products.jsx
@@ -1,4 +1,5 @@
 // import for thirdparty and external depencies
+import Navbar from "components/Navbars/AdminNavbar";
 import React from "react";
 
 // import for components
@@ -6,8 +7,21 @@ import React from "react";
 const Product = () => {
   return (
     <>
+      <Navbar transparent />
       <main>
-        <div className="relative pt-16 pb-32 flex content-center"></div>
+        <div className="relative pt-16 pb-32 flex content-center items-center justify-center min-h-screen-75">
+          <div
+            className="absolute top-0 w-full h-full bg-center bg-cover"
+            style={{
+              backgroundImage: `url("https://media.istockphoto.com/photos/forest-wooden-table-background-summer-sunny-meadow-with-green-grass-picture-id1353553203?b=1&k=20&m=1353553203&s=170667a&w=0&h=QTyTGI9tWQluIlkmwW0s7Q4z7R_IT8egpzzHjW3cSas=")`,
+            }}
+          >
+            <span
+              id="blackOverlay"
+              className="w-full h-full absolute opacity-75 bg-black"
+            ></span>
+          </div>
+        </div>
       </main>
     </>
   );
