5cf098263574cc33a59663c8b8ec006fa2fff51a
Wilson Ibekason <wanbekgithub@gmail.com>
2022-08-04 05:33:47 +0100

    defining logic tofetch individual products

--- src/oncontext/productContext/onEcomContext.js
+++ src/oncontext/productContext/onEcomContext.js
@@ -8,7 +8,19 @@ export const EcomProvider = ({ children }) => {
   const renderMoreProductsItems1 = ["M", "o", "r", "e"];
   const renderMoreProductsItems2 = ["p", "r", "o", "d", "u", "c", "t", "s"];
   const { productId } = useParams();
+  // DEFINE STATES
+  const [products, setProducts] = useState();
+  const [loading, setLoading] = useState(true);
+  const [productDetails, setProductDetails] = useState();
+  const [productDetailsMore, setProductDetailsMore] = useState();
+  const [productDetail, setProductDetail] = useState();
 
+  // DEFINE STATES
+
+  // fetching productDetails
+  const fetchProuctDetails = () => {
+    const query = productDetailQuery(productId);
+  };
   return (
     <EcomContext.Provider
       value={{ renderMoreProductsItems1, renderMoreProductsItems2 }}
