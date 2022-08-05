a5ec19509c8b0f9ba59ba595c5d34f6a87ea5baa
Wilson Ibekason <wanbekgithub@gmail.com>
2022-08-05 15:42:55 +0100

    edit post schema

--- makemaker/schemas/blogs/post.js
+++ makemaker/schemas/blogs/post.js
@@ -6,6 +6,12 @@ export default {
     {
       name: "title",
       title: "Title",
+      description: "keep title shop",
+      type: "string",
+    },
+    {
+      name: "description",
+      title: "Description",
       type: "string",
     },
     {
