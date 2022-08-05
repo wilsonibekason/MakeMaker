cec08546f6bdb13be232aa8f8bf2ebebc099928a
Wilson Ibekason <wanbekgithub@gmail.com>
2022-08-05 16:51:40 +0100

    initialise structure fro creating blog post

--- src/views/blog/BlogDetails.jsx
+++ src/views/blog/BlogDetails.jsx
@@ -1,6 +1,13 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const BlogDetails = () => {
+  let blogid;
+  let Id = useParams();
+  blogid = Id?.id;
+
+  ///// defining states
+
   return <div>BlogDetails</div>;
 };
 
