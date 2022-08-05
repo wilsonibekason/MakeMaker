256df691df4992a65b4dd7d875379d0f6fb7eee5
Wilson Ibekason <wanbekgithub@gmail.com>
2022-08-05 17:57:29 +0100

    fetched blog details and more blogs

--- src/views/blog/BlogDetails.jsx
+++ src/views/blog/BlogDetails.jsx
@@ -1,7 +1,7 @@
 import { useStateContextProduct } from "../../oncontext/productContext/onProductContext";
-import React from "react";
+import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { blogDetailQuery } from "../../utils/data";
+import { blogDetailMoreQuery, blogDetailQuery } from "../../utils/data";
 import { client } from "../../client";
 import { useStateBlogContext } from "../../oncontext/blogContext/OnBlogContext";
 
@@ -14,19 +14,47 @@ const BlogDetails = () => {
     setLoading,
     BlogDetails,
     setBlogDetails,
-  } = useStateBlogContextt();
+  } = useStateBlogContext();
   let blogid;
+  let query;
+  let queryMore;
   let Id = useParams();
   blogid = Id?.id;
 
   ///// defining states
   const fetchBlogDetails = () => {
-    let query;
+    setLoading(true);
+
     query = blogDetailQuery(blogid);
     if (query) {
-      client.fetch(query).then((data) => {});
+      client.fetch(query).then((data) => {
+        setBlogDetails(data);
+        setLoading(false);
+        if (data[0]) {
+          queryMore = blogDetailMoreQuery(data[0]);
+          client
+            .fetch(queryMore)
+            .then((moreData) => {
+              setMoreBlogs(moreData);
+              setLoading(false);
+            })
+            .catch((error) =>
+              console.log(error?.response?.body?.error?.description)
+            );
+        }
+      });
     }
   };
+
+  useEffect(() => {
+    fetchBlogDetails();
+  }, [blogid]);
+  // LOGS
+  console.log("====================================");
+  console.log(blogAuthor);
+  console.log(BlogDetails);
+  console.log(moreBlogs);
+  console.log("====================================");
   return <div>BlogDetails</div>;
 };
 
