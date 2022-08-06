6cd39d17576a823f95ea4e88e4a5a00a9ec8cd79
Wilson Ibekason <wanbekgithub@gmail.com>
2022-08-06 15:17:06 +0100

    add fetaure query and carousel pkg

--- src/views/blogs/section/FeaturedPosts.jsx
+++ src/views/blogs/section/FeaturedPosts.jsx
@@ -1,3 +1,4 @@
+import { client } from "../../../client";
 import React, { useState, useEffect } from "react";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
@@ -28,15 +29,25 @@ const responsive = {
 const FeaturedPosts = () => {
   const [featuredPosts, setFeaturedPosts] = useState([]);
   const [dataLoaded, setDataLoaded] = useState(false);
+  let query;
+  // fetch detailed queries
+  const fetchFeaturedPost = () => {
+    query = getFeaturedPosts();
+    client
+      .fetch(query)
+      .then((result) => {
+        setFeaturedPosts(result);
+        setDataLoaded(true);
+      })
+      .catch((error) => console.log(error?.response?.body?.error?.description));
+  };
 
   useEffect(() => {
-    getFeaturedPosts().then((result) => {
-      setFeaturedPosts(result);
-      setDataLoaded(true);
-    });
+    fetchFeaturedPost();
   }, []);
-
-  const customLeftArrow = (
+  /// logs
+  console.log(featuredPosts);
+  const customLeftArrows = (
     <div className="absolute arrow-btn left-0 text-center py-3 cursor-pointer bg-pink-600 rounded-full">
       <svg
         xmlns="http://www.w3.org/2000/svg"
@@ -78,7 +89,24 @@ const FeaturedPosts = () => {
     <div className="mb-8">
       <Carousel
         infinite
-        customLeftArrow={customLeftArrow}
+        customLeftArrow={
+          <div className="absolute arrow-btn left-0 text-center py-3 cursor-pointer bg-pink-600 rounded-full">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6 text-white "
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M10 19l-7-7m0 0l7-7m-7 7h18"
+              />
+            </svg>
+          </div>
+        }
         customRightArrow={customRightArrow}
         responsive={responsive}
         itemClass="px-4"
