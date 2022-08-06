9f8b418f77f0ba43f14aa48f50519a300609cfd4
Wilson Ibekason <wanbekgithub@gmail.com>
2022-08-06 11:45:54 +0100

    export all blog contents dynamically

---
+++ src/views/blogs/index.js
@@ -0,0 +1,11 @@
+export { default as Header } from "./Header";
+export { default as AdjacentPostCard } from "./AdjacentPostCard";
+export { default as Categories } from "./Categories";
+export { default as Comments } from "./Comments";
+export { default as CommentForm } from "./CommentForm";
+export { default as FeaturedPostCard } from "./FeaturedPostCard";
+export { default as Layout } from "./Layout";
+export { default as Loader } from "./Loader";
+export { default as PostCard } from "./PostCard";
+export { default as PostDetail } from "./PostDetail";
+export { default as PostWidget } from "./PostWidget";
