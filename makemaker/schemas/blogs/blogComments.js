dceb74b6d362364fa22c98e518a8344cab167be0
Wilson Ibekason <wanbekgithub@gmail.com>
2022-08-08 17:14:46 +0100

    complete create blog comment schema

---
+++ makemaker/schemas/blogs/blogComments.js
@@ -0,0 +1,45 @@
+import { GrContact } from "react-icons/gr";
+
+export default {
+  name: "blogComments",
+  title: "Blog Comments",
+  type: "document",
+  icon: GrContact,
+  fields: [
+    {
+      name: "fullName",
+      title: "FullName",
+      type: "text",
+      required: true,
+      display: {
+        placeholder: "Enter Name",
+        rules: [
+          {
+            required: true,
+            message: "Please enter a name",
+          },
+        ],
+      },
+    },
+    {
+      name: "email",
+      title: "Email",
+      type: "email",
+      required: true,
+      display: {
+        placeholder: "Enter Email",
+        rules: [
+          {
+            required: true,
+            message: "Please enter a valid email address",
+          },
+        ],
+      },
+    },
+    {
+      name: "message",
+      title: "Message",
+      type: "string",
+    },
+  ],
+};
--- src/views/blogs/CommentForm.jsx
+++ src/views/blogs/CommentForm.jsx
@@ -88,6 +88,7 @@ const CommentForm = () => {
                             value={message}
                           />
                         </div>
+
                         <div className="text-center mt-6">
                           <button
                             className="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
