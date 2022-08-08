f6432ae4d0737c618901686247655df0f1483d45
Wilson Ibekason <wanbekgithub@gmail.com>
2022-08-08 18:54:07 +0100

    complete export blog comment schema

---
+++ makemaker/schemas/blogs/comment.js
@@ -0,0 +1,12 @@
+export default {
+  name: "comment",
+  title: "Comment",
+  type: "document",
+  fields: [
+    {
+      name: "postedBy",
+      title: "Posted By",
+      type: "postedBY",
+    },
+  ],
+};
